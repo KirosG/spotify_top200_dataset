7ac59848a602ddc2c2bf9d4cb288e2f322a8b99b
Andrew <andrew.paster@gmail.com>
2018-02-10 22:40:05 -0800

    no legend

--- app.py
+++ app.py
@@ -22,25 +22,24 @@ colors = {'Spanish Speaking':'rgba(93, 165, 218, 1)',
 app = dash.Dash()
 
 app.layout = html.Div([
-    html.H1('Analysis of Spotify Data: When Songs Reach Their Peak'),
-    html.P('The analysis is based on a spotify dataset containing the top 200 songs played daily in each country.'),
-    html.P('The data is filtered for songs that were played at least once in all 54 countries'),
-    html.P('So the data represents big international hits.'),
-    html.P('The dashboard contains two charts: a bar chart showing the order in which the song hit peak by country. For instance, Ed Sheeran Shape of You first hit peak in Ireland on January 6th, 2017 with about 130k plays'),
-    html.P('Then it hit peak in the United Kingdom on January 9th with about 1.3 million plays'),
-    html.P('So you can get a feel for how a song traveled around the world gaining popularity in waves'),
-    html.P('The bar colors represent English speaking countries, Spanish speaking countries, and others'),
-    html.P('Although there is not always a discernible pattern with the bar colors, Despacito by Luis Fonsi presents an interesting case'),
-    html.P('The second chart shows the total global plays by date. For Ed Sheeran Shape of You, he released the song at the beginning of January and then the song was also on his March album release. So you can see how marketing plays into when people listen.'),
-    html.H3('Choose a Song from the Dropdown Menu'),
+    html.H1('Analysis of Spotify Data: When Songs Reach Their Peak', id='head-title'),
+    html.P('The analysis is based on a spotify dataset. The dataset contains daily number of song plays for the top 200 songs in 53 countries. The data set covers all of 2017.', className='intro-information'),
+    html.P('The data presented here has been filtered for songs that were played in all 53 countries represented in the data set. So if a song did not make it into the top 200 for all countries at least once, then the song was not included. Thus this visualization focuses on major international hits.', className='intro-information'),
+    html.P('The dashboard contains two charts: a bar chart showing the order in which the song reached peak by country. For instance, Ed Sheeran Shape of You was the most played song in the data set. The song first hit peak number of plays in Ireland on January 6th, 2017 with about 130k plays Then it hit peak in the United Kingdom on January 9th with about 1.3 million plays', className='intro-information'),
+    html.P('So you can get a feel for how a song traveled around the world gaining popularity in waves. The bar colors represent English speaking countries, Spanish speaking countries, and others. Although there is not always a discernible pattern with the bar colors, Despacito by Luis Fonsi presents an interesting case where clearly the song was popular in Spanish speaking countries before the song became popular around the world.', className='intro-information'),
+    html.P("The second chart shows the total global plays by date. For Ed Sheeran Shape of You, he released the song at the beginning of January and then the song was also on his March album release. So you can clearly see the two spikes, which might be evidence of two separate marketing pushes. The song spiked again on New Year's Eve The general shape of a song is that it gets peak plays early in the release and quickly fizzles out.", className='intro-information'),
+    html.H1('Choose a Song from the Dropdown Menu - Suggestion: Luis Fonsi Despacito', id='drop-down'),
     dcc.Dropdown(
-        id='song-chooser',
-        options=[{'label': i, 'value': i} for i in artists],
-        value='Ed Sheeran Shape of You'
-    ),
-    dcc.Graph(id='bar-chart'),
-    html.H2('Total Global Plays By Date'),
-    dcc.Graph(id='line-chart')
+            id='song-chooser',
+            options=[{'label': i, 'value': i} for i in artists],
+            value='Ed Sheeran Shape of You'
+        ),
+    html.Div([
+        html.H3('Order in which the Song Peaked'),
+        dcc.Graph(id='bar-chart')], id='chart-one'),
+    html.Div([
+        html.H3('Total Global Plays by Date'),
+        dcc.Graph(id='line-chart')], id='chart-two')
 ])
 
 @app.callback(Output('bar-chart', 'figure'), [Input('song-chooser', 'value')])
@@ -58,7 +57,17 @@ def update_graph(selected_dropdown_value):
             'text': dataframe.Date,
             'type': 'bar',
             'marker': dict(color = dataframe.Colors)
-        }]
+        }],
+        'layout': go.Layout(
+            xaxis={'title': 'Country', 'tickangle':45},
+            yaxis={'title': 'Number of Plays on Peak Day'},
+            margin={'l': 100, 'b': 125, 't': 40, 'r': 20},
+            legend={'x': 0, 'y': 1},
+            showlegend=True,
+            hovermode='closest',
+            title='Order in which ' + selected_dropdown_value + ' Peaked'
+        )
+
     }
 
 
@@ -87,9 +96,10 @@ def update_figure(selected_dropdown_value):
         'layout': go.Layout(
             xaxis={'title': 'Date'},
             yaxis={'title': 'Total Plays'},
-            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+            margin={'l': 100, 'b': 40, 't': 40, 'r': 10},
             legend={'x': 0, 'y': 1},
-            hovermode='closest'
+            hovermode='closest',
+            title='Total Global Plays by Date for ' + selected_dropdown_value + ' in 2017'
         )
     }
 
